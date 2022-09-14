1b62cd3de97060f821774d7a5e5c30bb08192817
gtithathonor <jeong.suyoung92@gmail.com>
2022-09-14 20:30:23 +0900

    javascript - jquery시작

---
+++ CH01/script.js
@@ -0,0 +1,13 @@
+let num = 1;
+let num2 = 1.5;
+let str = "안녕";
+let str2 = '안녕';
+let str3 = `내 번호는 ${num}`;
+let isOk = true;
+
+console.log("정수", num);
+console.log("실수", num2);
+console.log("문자열1", str);
+console.log("문자열2", str2);
+console.log("문자열3", str3);
+console.log("불리언", isOk);
